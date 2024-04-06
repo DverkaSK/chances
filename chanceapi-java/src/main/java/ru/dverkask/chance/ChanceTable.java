eae3ae6ce4e4a84b81f71c443f2f90fb1ea5d03b
DverkaSK <dverkask@gmail.com>
2024-04-07 00:48:12 +0300

    feat: make chance table iterable

--- chanceapi-java/src/main/java/ru/dverkask/chance/ChanceTable.java
+++ chanceapi-java/src/main/java/ru/dverkask/chance/ChanceTable.java
@@ -1,23 +1,37 @@
 package ru.dverkask.chance;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.*;
 
-public abstract class ChanceTable<T> {
+public abstract class ChanceTable<T> implements Iterable<T> {
     protected List<Chance<T>> chances = new ArrayList<>();
+    protected ChanceTable() {
 
-    public static <T> ChanceTable<T> create(ChanceType type) {
+    }
+
+    @SafeVarargs public static <T> ChanceTable<T> create(ChanceType type, Chance<T>... chances) {
         return switch (type) {
-            case PERCENTAGE -> new PercentageChanceTable<>();
-            case WEIGHT -> new WeightChanceTable<>();
+            case PERCENTAGE -> new PercentageChanceTable<>(chances);
+            case WEIGHT -> new WeightChanceTable<>(chances);
             default -> throw new IllegalArgumentException("Unknown type: " + type);
+        };
+    }
+
+    @NotNull @Override public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private final Iterator<Chance<T>> innerIterator = chances.iterator();
+            @Override public boolean hasNext() {
+                return innerIterator.hasNext();
+            }
+
+            @Override public T next() {
+                return innerIterator.next().item();
+            }
         };
     }
 
     public abstract void addChance(T item, double chance);
 
     public abstract T roll();
-
-    public enum ChanceType {
-        PERCENTAGE, WEIGHT
-    }
 }
